7d670e2510ebdbaafd07b8da529d31db584ebd93
unknown <justin.noname@gmail.com>
2020-06-24 14:08:57 -0400

    base javascript

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const app={};
 
+var tempC = 0;
+var tempCFeels = 0;
+var Weather = "";
+var WeatherDescription = "";
+var windSpeed =  0;
+
 app.innit = () =>{
     app.getWeather();
 }
@@ -11,10 +17,60 @@ app.getWeather = () =>{
         dataType: 'json',
     }).then(Result => {
         console.log("Results are: ", Result);
-        var htmlstring1=`
+        tempC = Result.main.temp - 273.15
+        tempC = parseFloat(tempC.toFixed(1));
+        tempCFeels = Result.main.feels_like -273.15
+        tempCFeels = parseFloat(tempCFeels.toFixed(1));
+        Weather = Result.weather[0].main;
+        WeatherDescription = Result.weather[0].description;
+        windSpeed = Result.wind.speed;
         
-        `
+        app.recommendations();
     })
+}
+
+app.recommendations = () =>{
+    var htmlTemp=`
+    <p>Temperature is ${tempC} but feels like: ${tempCFeels}<p>
+    `
+    $(".temp").append(htmlTemp);
+
+    
+    
+    console.log(Weather);
+    if(Weather == "Clouds")
+        {var htmlSkyImg=`
+        <img src="./assets/cloud.png" width="60%" height="60%"> 
+        `
+        $(".sky").append(htmlSkyImg);
+        }
+    else if (Weather == "Rain")
+    {var htmlSkyImg=`
+        <img src="./assets/rain.png" width="60%" height="60%">
+        `
+        $(".sky").append(htmlSkyImg);
+        }
+    else if (Weather == "Snow")
+    {var htmlSkyImg=`
+        <img src="./assets/snow.png" width="60%" height="60%">
+        `
+        $(".sky").append(htmlSkyImg);
+        }
+    else{
+        var htmlSkyImg=`
+        <img src="./assets/sun.png" width="60%" height="60%">
+            `
+        $(".sky").append(htmlSkyImg);
+            
+    }
+    
+    var htmlSky=`
+    <p> Weather is:  ${WeatherDescription}
+    `
+    $(".sky").append(htmlSky);
+
+
+    
 }
 
 
